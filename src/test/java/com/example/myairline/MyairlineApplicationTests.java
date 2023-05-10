710686aa80f961b571c68c4ad4e0a55c90d61e81
ue <jaydeeppatil63110@gmail.com>
2023-05-10 13:08:56 +0530

    initial version

---
+++ src/test/java/com/example/myairline/MyairlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myairline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyairlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
